ba5b008008f16655ca962039dff894e6b635212b
francielydbastos <francielydbastos@gmail.com>
2022-06-13 10:07:15 -0300

    Banco de Dados 3 Task 0

---
+++ 0x02/java_jdbc_manipulation/src/Cliente.java
@@ -0,0 +1,47 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private int idade;
+    private String CPF;
+    private String RG;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+}
